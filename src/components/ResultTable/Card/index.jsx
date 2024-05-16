ca8f9a60e739855b4b4ef6cec528983ef273c0b6
Martin Solorio <msolorio@berkeley.edu>
2024-05-16 16:25:24 -0700

    Added 'Add to Cart' button styles

--- src/components/ResultTable/Card/index.jsx
+++ src/components/ResultTable/Card/index.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import { Detail } from './Detail';
 import { SearchContext } from '../../../contexts/SearchContext';
 import './Card.css';
 
@@ -19,15 +18,12 @@ function Card({ image, title, price, description }) {
             <div className='ProductImageContainer'>
                 <img src={image} alt={title} />
             </div>
-            <Detail
-                title={title}
-                price={price}
-            />
             {/* Add the rating stars based on rate value */}
             <div className="rating-stars">
                 {/* Render your rating stars component here */}
                 {/* Example: <RatingStars rating={rating} /> */}
             </div>
+            <button className="AddToCartButton">Add to Cart</button> {/* Add this button */}
         </div>
     );
 }
